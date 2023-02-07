c1bcce7250a533bf8ca7a9a14948adc0ec9fe66e
WybsonSantana <wybson@hotmail.com>
2023-02-06 21:41:28 -0300

    M2A12 - Reaproveitando cenários - Cucumber Expressions

--- src/test/java/br/dev/s2w/cucumber/AprenderCucumberStepdefs.java
+++ src/test/java/br/dev/s2w/cucumber/AprenderCucumberStepdefs.java
@@ -5,6 +5,10 @@ import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class AprenderCucumberStepdefs {
 
     @Dado("que criei o arquivo corretamente")
@@ -23,19 +27,53 @@ public class AprenderCucumberStepdefs {
     private int contador = 0;
 
     @Dado("que o valor do contador é {int}")
-    public void queOValorDoContadorE(Integer int1) {
-        contador = int1;
+    public void queOValorDoContadorE(Integer valorContador) {
+        contador = valorContador;
     }
 
     @Quando("eu incrementar em {int}")
-    public void euIncrementarEm(Integer int1) {
-        contador += int1;
+    public void euIncrementarEm(Integer incremento) {
+        contador += incremento;
     }
 
     @Entao("o valor do contador será {int}")
-    public void oValorDoContadorSera(Integer int1) {
-        Assert.assertEquals(Integer.parseInt(String.valueOf(int1)), contador);
+    public void oValorDoContadorSera(Integer valorContador) {
+        Assert.assertEquals(Integer.parseInt(String.valueOf(valorContador)), contador);
     }
 
+    private Calendar dataEntrega = Calendar.getInstance();
+    private DateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+
+    @Dado("que a entrega é dia {int}\\/{int}\\/{int}")
+    public void queAEntregaEDia(int dia, int mes, int ano) {
+        dataEntrega.set(Calendar.DAY_OF_MONTH, dia);
+        dataEntrega.set(Calendar.MONTH, mes - 1);
+        dataEntrega.set(Calendar.YEAR, ano);
+    }
+
+    @Quando("a entrega atrasar em {int} {word}")
+    public void aEntregaAtrasarEmDias(int periodo, String tempo) {
+        if (tempo.equals("dia") || tempo.equals("dias")) {
+            dataEntrega.add(Calendar.DAY_OF_MONTH, periodo);
+        }
+
+        if (tempo.equals("mês") || tempo.equals("meses")) {
+            dataEntrega.add(Calendar.MONTH, periodo);
+        }
+    }
+
+    @Entao("a entrega será efetuada em {int}\\/{int}\\/{int}")
+    public void aEntregaSeraEfetuadaEm(int dia, int mes, int ano) {
+        Calendar novaDataEntrega = Calendar.getInstance();
+
+        novaDataEntrega.set(Calendar.DAY_OF_MONTH, dia);
+        novaDataEntrega.set(Calendar.MONTH, mes - 1);
+        novaDataEntrega.set(Calendar.YEAR, ano);
+
+        Assert.assertEquals(
+                date.format(novaDataEntrega.getTime()),
+                date.format(dataEntrega.getTime())
+        );
+    }
 
 }
--- src/test/java/br/dev/s2w/cucumber/aprender_cucumber.feature
+++ src/test/java/br/dev/s2w/cucumber/aprender_cucumber.feature
@@ -7,12 +7,31 @@ Funcionalidade: Aprender Cucumber
   Eu quero aprender a utilizar Cucumber
   Para que eu possa automatizar critérios de aceitação
 
-Cenario: Deve executar especificação
+  Cenario: Deve executar especificação
     Dado que criei o arquivo corretamente
     Quando executá-lo
     Entao a especificação deve finalizar com sucesso
 
-Cenario: Deve incrementar contador
-  Dado que o valor do contador é 15
-  Quando eu incrementar em 3
-  Entao o valor do contador será 18
+  @CenárioContador1
+  Cenario: Deve incrementar contador
+    Dado que o valor do contador é 15
+    Quando eu incrementar em 3
+    Entao o valor do contador será 18
+
+  @CenárioContador2
+  Cenario: Deve incrementar contador
+    Dado que o valor do contador é 123
+    Quando eu incrementar em 35
+    Entao o valor do contador será 158
+
+  @CenárioAtrasoEntrega1
+  Cenario: Deve calcular atraso na entrega
+    Dado que a entrega é dia 05/04/2023
+    Quando a entrega atrasar em 2 dias
+    Entao a entrega será efetuada em 07/04/2023
+
+  @CenárioAtrasoEntrega2
+  Cenario: Deve calcular atraso na entrega da China
+    Dado que a entrega é dia 05/04/2023
+    Quando a entrega atrasar em 2 meses
+    Entao a entrega será efetuada em 05/06/2023
